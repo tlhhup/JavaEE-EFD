60e886c336a3b3598f4cfc44433981dc5faf8572
tlhhup <137273278@qq.com>
2020-06-14 17:13:28 +0800

    1. 处理第二章遗留代码

--- j2e-chapter-2/ssm/src/main/java/com/woniuxy/ssm/mapper/UserMapper.java
+++ j2e-chapter-2/ssm/src/main/java/com/woniuxy/ssm/mapper/UserMapper.java
@@ -1,8 +1,7 @@
 package com.woniuxy.ssm.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
-
 import com.woniuxy.ssm.entity.User;
+import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMapper {
